774eba190b8a701eeac8aa73ef939c8aa4042c5d
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-08-21 13:09:30 +0530

    Add files via upload

---
+++ 1_Datatypes,variables,constant/2_printf1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    int a = 10;
+    
+    printf("%d",a);
+
+    return 0;
+}
---
+++ 1_Datatypes,variables,constant/3_printf2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    int a = 10;
+    
+    printf("The value of a is %d",a);
+
+    return 0;
+}
