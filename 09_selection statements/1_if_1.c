7ce5f5ece217c1a2a920fc0fd58ca0199a97af88
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-09-07 10:20:48 +0530

    Create 1_if_1.c

---
+++ 09_selection statements/1_if_1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/*
+if(condition) 
+{
+   // Statements to execute if
+   // condition is true
+}*/
+
+int main()
+{
+    int a = 5, b = 10;
+
+    if(a > 10 && a > b)     //check condition
+    {
+        printf("a is less than b");
+    }
+     // if block is only enclosed inside parenthesis
+
+    return 0;
+}
