f215bd7776dcadadc3be54c7a9d228a5ae885e2f
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2023-02-13 03:26:00 +0530

    Update 11_function_pointer.c

--- 05_pointers/11_function_pointer.c
+++ 05_pointers/11_function_pointer.c
@@ -2,23 +2,40 @@
 
 //function pointer stores the address of the function
 
-int (*fp)(int,int);
-int maximum(int,int);
+
+int maximum(int a, int b,int c)
+{
+    if(a>b && a>c)
+        return a;
+    else if(b>c)
+        return b;
+    else 
+        return c;
+}
+
+int minimum(int a, int b, int c)
+{
+    if(a<b && a<c)
+        return a;
+    else if(b<c)
+        return b;
+    else 
+        return c;
+}
+
 
 int main()
 {
-    int a = 10, b = 20,c = 0;   //function pointer prototype
+    int a = 10, b = 20, c = 15;   
+
+    int (*fp)(int,int,int); //function pointer prototype
     
     fp = maximum;           //assigning address of function to pointer
+    printf("maximum : %d",(fp)(a,b,c));    
 
-    c = fp(a,b);    //calling function using pointer
+    fp = minimum;           //assigning address of function to pointer
+    printf("\nminimum : %d",(fp)(a,b,c));  
 
-    printf("%d",c);    
     return 0;
-}
-
-int maximum(int a, int b)
-{
-    return a + b;
 }
 
